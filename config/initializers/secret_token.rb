170b2b6237384867066a0eb97524b7292ec694d6
Dmitriy Kovrihin <agnostic.88@yandex.ru>
2014-01-14 06:32:39 +0200

    Should include secret_token.rb in repo, otherwise test will not run

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,2 @@
+#
+KPKMarsService::Application.config.secret_token = '6d70a6d4030d935f4c55ed10f48d1af500c254c18ea263213d6634aaa4c769a2b3a14a9b6b61fc05651b8dae66953f3a62361d51525c9308327a71cb7000dcf9'
